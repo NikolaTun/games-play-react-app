83119468cbab786bf41f88edbb7aa834cc86c326
nikolatun <niki_tc@abv.bg>
2023-02-07 15:51:17 +0200

    Register functionality added

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,32 @@
+import * as authService from "../../services/authService";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { authContext } from "../../contexts/authContext";
+
 const Register = () => {
+  const { userLogin } = useContext(authContext);
+  const navigate = useNavigate();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const { email, password, confirmPassword } = Object.fromEntries(
+      new FormData(e.target)
+    );
+
+    if (password !== confirmPassword) {
+      return;
+    }
+
+    authService.register(email, password).then((authData) => {
+      userLogin(authData);
+      navigate("/");
+    });
+  };
+
   return (
     <section id="register-page" className="content auth">
-      <form id="register">
+      <form onSubmit={onSubmit} id="register">
         <div className="container">
           <div className="brand-logo" />
           <h1>Register</h1>
@@ -15,11 +40,7 @@ const Register = () => {
           <label htmlFor="pass">Password:</label>
           <input type="password" name="password" id="register-password" />
           <label htmlFor="con-pass">Confirm Password:</label>
-          <input
-            type="password"
-            name="confirm-password"
-            id="confirm-password"
-          />
+          <input type="password" name="confirmPassword" id="confirmPassword" />
           <input className="btn submit" type="submit" defaultValue="Register" />
           <p className="field">
             <span>
